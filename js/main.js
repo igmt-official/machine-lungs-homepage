aa2bb856781d47f10f9ad3906825d98f0e49221a
igmt.python <igmtp.python@gmail.com>
2022-04-23 08:50:24 +0800

    Updated.

--- js/main.js
+++ js/main.js
@@ -94,7 +94,7 @@ const selectedIcon = localStorage.getItem("selected-icon");
 const getCurrentTheme = () =>
   document.body.classList.contains(darkTheme) ? "dark" : "light";
 const getCurrentIcon = () =>
-  themeButton.classList.contains(iconTheme) ? "ri-moon-line" : "ri-sun-line";
+  themeButton.classList.contains(iconTheme) ? "ri-sun-line" : "ri-moon-line";
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
